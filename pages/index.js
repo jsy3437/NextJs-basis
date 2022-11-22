d13d210f7bc531dd965fa8db7ba3806026fa9d3c
Uni <joseoyun12@gmail.com>
2022-11-22 20:03:19 +0900

    영화 API 요청, 받은 데이터 띄우는 코드 추가함

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
+import { useEffect, useState } from 'react';
 import Seo from '../component/Seo';
 
+const API_KEY = '7384e6d100f7fbc482c7d9945d06b2b3';
+
 export default function Home() {
+	const [movies, setMovies] = useState([]);
+	useEffect(() => {
+		(async () => {
+			const { results } = await (
+				await fetch(
+					`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+				)
+			).json();
+			setMovies(results);
+		})();
+	}, []);
+	console.log(movies);
 	return (
 		<div>
 			<Seo title="Home" />
-			<h1>Hello</h1>
+			{!movies && <h4>Loading...</h4>}
+			{movies?.map((movie) => (
+				<div key={movie.id}>
+					<h4>{movie.original_title}</h4>
+				</div>
+			))}
 		</div>
 	);
 }
