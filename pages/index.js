dfc37425c2444e231c91cf7a60d6961aab889a03
Uni <joseoyun12@gmail.com>
2022-11-22 20:08:16 +0900

    API에서 받아온 데이터 중 이미지 띄워주고 스타일링함

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ const API_KEY = '7384e6d100f7fbc482c7d9945d06b2b3';
 
 export default function Home() {
 	const [movies, setMovies] = useState([]);
+
 	useEffect(() => {
 		(async () => {
 			const { results } = await (
@@ -15,16 +16,38 @@ export default function Home() {
 			setMovies(results);
 		})();
 	}, []);
-	console.log(movies);
+
 	return (
-		<div>
+		<div className="container">
 			<Seo title="Home" />
 			{!movies && <h4>Loading...</h4>}
 			{movies?.map((movie) => (
-				<div key={movie.id}>
+				<div className="movie" key={movie.id}>
+					<img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
 					<h4>{movie.original_title}</h4>
 				</div>
 			))}
+			<style jsx>{`
+				.container {
+					display: grid;
+					grid-template-columns: 1fr 1fr;
+					padding: 20px;
+					gap: 20px;
+				}
+				.movie img {
+					max-width: 100%;
+					border-radius: 12px;
+					transition: transform 0.2s ease-in-out;
+					box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+				}
+				.movie:hover img {
+					transform: scale(1.05) translateY(-10px);
+				}
+				.movie h4 {
+					font-size: 18px;
+					text-align: center;
+				}
+			`}</style>
 		</div>
 	);
 }
