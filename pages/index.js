2f2d5aa5136b7cc98d865d12cb817c9593a57b98
Uni <joseoyun12@gmail.com>
2022-11-24 11:46:34 +0900

    서버에서 API 요청 작업을 실행하고 pageProps로 넘어가게 변경함

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,15 @@
-import { useEffect, useState } from 'react';
 import Seo from '../component/Seo';
 
-const API_KEY = '7384e6d100f7fbc482c7d9945d06b2b3';
-
-export default function Home() {
-	const [movies, setMovies] = useState([]);
-
-	useEffect(() => {
-		(async () => {
-			const { results } = await (
-				await fetch(
-					`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-				)
-			).json();
-			setMovies(results);
-		})();
-	}, []);
-
+export default function Home({ results }) {
 	return (
 		<div className="container">
 			<Seo title="Home" />
-			{!movies && <h4>Loading...</h4>}
-			{movies?.map((movie) => (
+			{results?.map((movie) => (
 				<div className="movie" key={movie.id}>
-					<img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+					<img
+						src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+						alt="movie poster image"
+					/>
 					<h4>{movie.original_title}</h4>
 				</div>
 			))}
@@ -50,4 +36,15 @@ export default function Home() {
 			`}</style>
 		</div>
 	);
+}
+
+export async function getServerSideProps() {
+	const { results } = await (
+		await fetch('http://localhost:3000/api/movies')
+	).json();
+	return {
+		props: {
+			results,
+		},
+	};
 }
