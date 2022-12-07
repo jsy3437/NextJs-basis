7d78a4ce08dc0bd6aebc902f560f68a2087a0c95
Uni <joseoyun12@gmail.com>
2022-12-07 23:50:41 +0900

    영화카드 클릭시 디테일 페이지로 넘어갈 수 있게 처리함

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,53 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import Seo from '../component/Seo';
 
 export default function Home({ results }) {
+	const router = useRouter();
+	const onClick = (id) => {
+		router.push(`/movies/${id}`);
+	};
+
 	return (
 		<div className="container">
 			<Seo title="Home" />
 			{results?.map((movie) => (
-				<div className="movie" key={movie.id}>
+				<div onClick={() => onClick(movie.id)} className="movie" key={movie.id}>
 					<img
 						src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
 						alt="movie poster image"
 					/>
-					<h4>{movie.original_title}</h4>
+					<Link href={`/movies/${movie.id}`} legacyBehavior>
+						<a>{movie.original_title}</a>
+					</Link>
 				</div>
 			))}
-			<style jsx>{`
-				.container {
-					display: grid;
-					grid-template-columns: 1fr 1fr;
-					padding: 20px;
-					gap: 20px;
-				}
-				.movie {
-					cursor: pointer;
-				}
-				.movie img {
-					max-width: 100%;
-					border-radius: 12px;
-					transition: transform 0.2s ease-in-out;
-					box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-				}
-				.movie:hover img {
-					transform: scale(1.05) translateY(-10px);
-				}
-				.movie h4 {
-					font-size: 18px;
-					text-align: center;
-				}
-			`}</style>
+			<style jsx>
+				{`
+					.container {
+						display: grid;
+						grid-template-columns: 1fr 1fr;
+						padding: 20px;
+						gap: 20px;
+					}
+					.movie {
+						cursor: pointer;
+					}
+					.movie img {
+						max-width: 100%;
+						border-radius: 12px;
+						transition: transform 0.2s ease-in-out;
+						box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+					}
+					.movie:hover img {
+						transform: scale(1.05) translateY(-10px);
+					}
+					.movie h4 {
+						font-size: 18px;
+						text-align: center;
+					}
+				`}
+			</style>
 		</div>
 	);
 }
